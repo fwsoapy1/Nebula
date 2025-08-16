e80c98dde0d2e0cd34d1a9a29a361c4ef6249c54
fwsoapy1 <austin44larson1@gmail.com>
2025-08-16 16:17:10 -0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,15 @@
+local Players = game:GetService("Players")
+local LP = Players.LocalPlayer
+local pid = game.PlaceId
 
+if pid == 286090429 or pid == 301549746 or pid == 11746368176 then
+loadstring(game:HttpGet("raw.githubusercontent.com/fwsoapy1/Nebula/refs/heads/main/3games.lua"))()
+elseif pid == 8204899140 then
+    print("not done yet")
+elseif pid == 17625359962 or pid == 17516596118 then
+    print("not done yet")
+elseif pid == 3823781113 then
+    print("not done yet")
+else
+    LP:Kick("Nebula | Game Not Supported")
+end
